eb8a7a2c21cd6449c4fba7884bea5b8d5f468626
Alex Clark <aclark.xyz@gmail.com>
2016-06-30 15:03:53 -0700

    Added protein.ttl

--- src/com/cdd/bao/template/ModelSchema.java
+++ src/com/cdd/bao/template/ModelSchema.java
@@ -50,6 +50,8 @@ public class ModelSchema
 	public static final String PFX_UO = "http://purl.org/obo/owl/UO#";
 	public static final String PFX_DTO = "http://www.drugtargetontology.org/dto/";
 	public static final String PFX_GENEID = "http://www.bioassayontology.org/geneid#";
+	public static final String PFX_TAXON = "http://www.bioassayontology.org/taxon#";
+	public static final String PFX_PROTEIN = "http://www.bioassayontology.org/protein#";
 
 	public static final String BAT_ROOT = "BioAssayTemplate"; // root should be one of these, as well as a group
 	public static final String BAT_ASSAY = "BioAssayDescription"; // there should be zero-or-more of these in the schema file
