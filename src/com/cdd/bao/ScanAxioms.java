cabd532c9b646e620d460acbfec10f8cfd55ede4
Alex Clark <aclark.xyz@gmail.com>
2017-03-28 16:24:25 -0700

    Axioms down to CAT terms

--- src/com/cdd/bao/ScanAxioms.java
+++ src/com/cdd/bao/ScanAxioms.java
@@ -26,6 +26,8 @@ import com.cdd.bao.editor.*;
 public class ScanAxioms 
 {
 	private OntModel ontology; 
+	private Schema schema;
+	private SchemaVocab schvoc;
 	private Map<String, String> uriToLabel = new HashMap<String, String>();
 
 	public ScanAxioms()
@@ -34,21 +36,16 @@ public class ScanAxioms
 
 	public void exec() throws OntologyException 
 	{ 
-		/*
-		//assayKit, bioassay, bioassaySpecification, biologicalProcess, modeOfAction, molecular function
-		String irrelevantIRIs[] = 
-		{
-			"http://www.bioassayontology.org/bao#BAO_0000248",
-			"http://www.bioassayontology.org/bao#BAO_0000015", 
-			"http://www.bioassayontology.org/bao#BAO_0000026",
-			"http://www.bioassayontology.org/bao#BAO_0000264",
-			"http://www.bioassayontology.org/bao#BAO_0000074",
-			"http://www.bioassayontology.org/bao#BAO_0002202",
-			"http://www.bioassayontology.org/bao#BAO_0003075"
-		};*/
-					 
 	 	try 
 	  	{
+	  		Util.writeln("Loading common assay template...");
+	  		schema = ModelSchema.deserialise(new File("data/template/schema.ttl"));
+	  		
+	  		Util.writeln("Loading vocabulary dump...");
+	  		InputStream idump = new FileInputStream("data/template/vocab.dump");
+	  		schvoc = SchemaVocab.deserialise(idump, new Schema[]{schema});
+	  		idump.close();
+	  	
 	  		List<File> files = new ArrayList<>();
 	  		for (File f : new File("data/ontology").listFiles()) if (f.getName().endsWith(".owl")) files.add(f);
 	  		//for (File f : new File("data/preprocessed").listFiles()) if (f.getName().endsWith(".owl")) files.add(f);
@@ -64,19 +61,12 @@ public class ScanAxioms
 				ontology.read(rdr, null);
 				rdr.close();
 			}
-				  	
-	  		/* one file approach
-   			//ontology path
-   			String path = "/Users/handemcginty/home/"; 
-   
-   			in = new FileInputStream(path + "bao_complete_merged.owl"); 
-
-   			ontology = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_RDFS_INF); 
-   			ontology = (OntModel) ontology.read(in, "http://www.bioassayontology.org/bao/bao_complete_merged.owl");  
-   			in.close(); 
-   			System.out.println("Read BAO merged ontology");*/
    		}
    		catch (Exception e) {throw new OntologyException(e.getMessage());}
+		
+		Util.writeln("Collating values from schema...");
+		Set<String> schemaValues = new HashSet<>();
+		for (SchemaVocab.StoredTree stored : schvoc.getTrees()) for (SchemaTree.Node node : stored.tree.getFlat()) schemaValues.add(node.uri);
 		
 		Util.writeln("Read complete: counting triples...");
 		long timeThen = new Date().getTime();
@@ -156,6 +146,9 @@ public class ScanAxioms
 		int maxCardinalityCounter = 0;
 		int minCardinalityCounter = 0;
 
+		Map<String, Integer> propTypeCount = new TreeMap<>();
+
+		timeThen = new Date().getTime();
 		for (Iterator<OntClass> it = ontology.listClasses(); it.hasNext();)
 		{
 			// pull out the ontology sequence for this axiom:
@@ -165,74 +158,105 @@ public class ScanAxioms
 			//    v = value required by the axiom
 		
 			OntClass o = it.next();
+			if (!schemaValues.contains(o.asResource().getURI())) continue;
 			
 			for (Iterator<OntClass> i = o.listSuperClasses(); i.hasNext();)
 			{
 				OntClass c = i.next();
+				
 				if (c.isRestriction()) //go over each axiom of a particular class and put the class and axioms to the bag
-				{ //uriToLabel.getOrDefault(uri, ModelSchema.collapsePrefix(uri));
+				{
 					Restriction r = c.asRestriction(); //restriction == axiom
 					if (r.isAllValuesFromRestriction()) // only axioms
 					{ 
 						AllValuesFromRestriction av = r.asAllValuesFromRestriction();
 						OntProperty p = (OntProperty)av.getOnProperty();
+						String pname = nameNode(p);
 						OntClass v = (OntClass)av.getAllValuesFrom();
+
+						Resource[] sequence = expandSequence(v);
+						boolean anySchema = false;
+						for (Resource s : sequence) if (schemaValues.contains(s.getURI())) {anySchema = true; break;}
+						if (!anySchema) continue;
 						
 						String key = nameNode(o);
-						String val = "ALL: property=[" + nameNode(p) + "] value=[" + nameNode(v) + "]";
+						String val = "ALL: property=[" + pname + "] value=";
+						if (sequence.length == 0) val += "{nothing}";
+						for (int n = 0; n < sequence.length; n++) val += (n > 0 ? "," : "") + "[" + nameNode(sequence[n]) + "]";
+						
 						putAdd(axioms, key, val);
 						forAllCounter++;
+						propTypeCount.put(pname, propTypeCount.getOrDefault(pname, 0) + 1);
 					}
 					else if (r.isSomeValuesFromRestriction())
 					{
 						SomeValuesFromRestriction av = r.asSomeValuesFromRestriction();
 						OntProperty p = (OntProperty)av.getOnProperty();
+						String pname = nameNode(p);
 						OntClass v = (OntClass)av.getSomeValuesFrom();
 
+						Resource[] sequence = expandSequence(v);
+						boolean anySchema = false;
+						for (Resource s : sequence) if (schemaValues.contains(s.getURI())) {anySchema = true; break;}
+						if (!anySchema) continue;
+
 						String key = nameNode(o);
-						String val = "SOME: property=[" + nameNode(p) + "] value=[" + nameNode(v) + "]";
+						String val = "SOME: property=[" + pname + "] value=";
+						if (sequence.length == 0) val += "{nothing}";
+						for (int n = 0; n < sequence.length; n++) val += (n > 0 ? "," : "") + "[" + nameNode(sequence[n]) + "]";
+
 						putAdd(axioms, key, val);
 						forSomeCounter++;
+						propTypeCount.put(pname, propTypeCount.getOrDefault(pname, 0) + 1);
 					}
 					else if (r.isMaxCardinalityRestriction())
 					{
 						MaxCardinalityRestriction av = r.asMaxCardinalityRestriction();
 						OntProperty p = (OntProperty)av.getOnProperty();
+						String pname = nameNode(p);
 						int maximum = av.getMaxCardinality();
 						
 						String key = nameNode(o);
-						String val = "MAX: property=[" + nameNode(p) + "] maximum=" + maximum;
+						String val = "MAX: property=[" + pname + "] maximum=" + maximum;
 						putAdd(axioms, key, val);
 						maxCardinalityCounter++;
+						propTypeCount.put(pname, propTypeCount.getOrDefault(pname, 0) + 1);
 					}
 					else if (r.isMinCardinalityRestriction())
 					{
 						MinCardinalityRestriction av = r.asMinCardinalityRestriction();
 						OntProperty p = (OntProperty)av.getOnProperty();
+						String pname = nameNode(p);
 						int minimum = av.getMinCardinality();
 
 						String key = nameNode(o);
-						String val = "MIN: property=[" + nameNode(p) + "] minimum=" + minimum;
+						String val = "MIN: property=[" + pname + "] minimum=" + minimum;
 						putAdd(axioms, key, val);
 						minCardinalityCounter++;
+						propTypeCount.put(pname, propTypeCount.getOrDefault(pname, 0) + 1);
 					}
 					else if (r.isCardinalityRestriction())
 					{
 						CardinalityRestriction av = r.asCardinalityRestriction();
 						OntProperty p = (OntProperty)av.getOnProperty();
+						String pname = nameNode(p);
 						int cardinality = av.getCardinality();
 
 						String key = nameNode(o);
-						String val = "EQ: property=[" + nameNode(p) + "] cardinarlity=" + cardinality;
+						String val = "EQ: property=[" + pname + "] cardinality=" + cardinality;
 						putAdd(axioms, key, val);
 						cardinalityCounter++;
+						propTypeCount.put(pname, propTypeCount.getOrDefault(pname, 0) + 1);
 					}
 				}
 			}
+			
+			long timeNow = new Date().getTime();
+			if (timeNow > timeThen + 2000) {Util.writeln("    so far: " + axioms.size()); timeThen = timeNow;}
 		}
 		
 		Util.writeln("\n---- Category Counts ----");
-		Util.writeln("total axioms: " + axioms.size());
+		Util.writeln("terms with axioms: " + axioms.size());
 		Util.writeln("for all axioms: " + forAllCounter);
 		Util.writeln("for some axioms: " + forSomeCounter);
 		Util.writeln("for max axioms: " + maxCardinalityCounter);
@@ -246,7 +270,11 @@ public class ScanAxioms
 		try
 		{
 			PrintWriter wtr = new PrintWriter(f);
-		
+
+			wtr.println("---- Property Counts ----");
+			for (String key : propTypeCount.keySet()) wtr.println("[" + key + "] count=" + propTypeCount.get(key));
+
+			wtr.println("\n---- Axioms ----");		
 			for (String key : axioms.keySet())
 			{
 				wtr.println("[" + key + "]:");
@@ -288,4 +316,26 @@ public class ScanAxioms
 		return "{???}";
 	}
 	
+	// takes a resource that may well be an anonymous value: which means go through and expand it out into a chain of resources
+	private Resource[] expandSequence(Resource v)
+	{					
+		if (!v.isAnon()) return new Resource[]{v};
+
+		Property rdfType = ontology.createProperty(ModelSchema.PFX_RDF + "type");
+		List<Resource> sequence = new ArrayList<>(), anonymous = new ArrayList<>();
+		anonymous.add(v);
+		while (anonymous.size() > 0)
+		{
+			for (StmtIterator iter = ontology.listStatements(anonymous.remove(0), null, (RDFNode)null); iter.hasNext();)
+			{
+				Statement stmt = iter.next();
+				Property prop = stmt.getPredicate();
+				if (prop.equals(rdfType)) continue;
+				RDFNode object = stmt.getObject();
+				if (!object.isResource()) continue;
+				if (!object.isAnon()) sequence.add(object.asResource()); else anonymous.add(object.asResource());
+			}
+		}
+		return sequence.toArray(new Resource[sequence.size()]);
+	}
 }
