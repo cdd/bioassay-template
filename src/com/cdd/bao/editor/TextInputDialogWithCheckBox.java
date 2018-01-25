6b6f9651e1b60ddd0658059372c4357d69d32ae6
Kode Charlie <kodecharlie@yahoo.com>
2018-01-24 17:32:48 -0800

    Issue #39: per Peter’s comments, moving TextInputDialogWithCheckBox to other package (com.cdd.bao.editor) so as not to break docker build.

--- src/com/cdd/bao/editor/TextInputDialogWithCheckBox.java
+++ src/com/cdd/bao/editor/TextInputDialogWithCheckBox.java
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-package com.cdd.bao.util;
+package com.cdd.bao.editor;
 
 import javafx.application.*;
 import javafx.beans.*;
