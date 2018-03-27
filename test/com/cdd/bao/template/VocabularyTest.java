e826f2b1f02db57e7e1208f9c7b97633aae8789b
Kode Charlie <kodecharlie@yahoo.com>
2018-03-27 14:02:11 -0700

    Issue #756: drafted several unit-tests for directives used in ontologies.

--- test/com/cdd/bao/template/VocabularyTest.java
+++ test/com/cdd/bao/template/VocabularyTest.java
@@ -26,8 +26,11 @@ import static org.junit.Assume.*;
 
 import java.io.*;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.*;
 
+import com.cdd.bao.template.Vocabulary.*;
 import com.cdd.bao.util.*;
 
 /*
@@ -36,32 +39,82 @@ import com.cdd.bao.util.*;
 
 public class VocabularyTest
 {
-	private File testDataDir;
-	
-	@Before
-	public void setup()
-	{
-		testDataDir = new File(System.getProperty("user.dir") + "/build/test/testData");
-	}
-
 	@Test
-	public void testLoadTestOntology() throws IOException
+	public void testRemap() throws IOException
 	{
+		File testRemapDir = new File(System.getProperty("user.dir") + "/build/test/testData/remapTo");
 		Vocabulary vocab = new Vocabulary();
-		vocab.load(testDataDir.getCanonicalPath(), null);
-		
+		vocab.load(testRemapDir.getCanonicalPath(), null);
+
+		boolean foundClassHier = false;
 		Vocabulary.Hierarchy hier = vocab.getValueHierarchy();
 		for (Vocabulary.Branch branch : hier.rootBranches)
 		{
 			assertTrue("root label should contain \"Foo\"", (branch.label != null && branch.label.contains("Foo")));
 			assertTrue("remapping should result in a single child branch", branch.children.size() == 1);
 
-			for (Vocabulary.Branch child : branch.children)
+			Vocabulary.Branch child = branch.children.get(0);
+			assertTrue("child label should contain \"Bar\"", (child.label != null && child.label.contains("Bar")));
+
+			foundClassHier = true;
+		}
+		if (!foundClassHier) throw new AssertionError("Expected class hierarchy NOT FOUND!");
+	}
+
+	@Test
+	public void testEquivalence() throws IOException
+	{
+		File testEquivDir = new File(System.getProperty("user.dir") + "/build/test/testData/equivalenceClass");
+		Vocabulary vocab = new Vocabulary();
+		vocab.load(testEquivDir.getCanonicalPath(), null);
+
+		boolean foundClassHier = false;
+		Vocabulary.Hierarchy hier = vocab.getValueHierarchy();
+		for (Vocabulary.Branch branch : hier.rootBranches)
+		{
+			Util.writeln("branch.label=" + branch.label + "; branch.children.size=" + branch.children.size());
+			if (branch.label != null && branch.label.equals("A1"))
 			{
-				assertTrue("root should have child branch", child.label != null);
-				assertTrue("child label should contain \"Bar\"", (child.label.contains("Bar")));
+				// assertTrue("equivalence should result in a single parent", branch.children.size() == 2);
+				Vocabulary.Branch c1 = branch.children.get(0);
+				Util.writeln("c1.label=" + c1.label);
+				// Vocabulary.Branch c2 = branch.children.get(1);
+				//assertTrue("B1 and B2 should have same parent",
+				//	(c1.label != null && c1.label.equals("B1") && c2.label != null && c2.label.equals("B2")));
+
+				foundClassHier = true;
 			}
 		}
-		assertTrue("Test", true);
+		if (!foundClassHier) throw new AssertionError("Expected class hierarchy NOT FOUND!");
+	}
+
+	@Test
+	public void testNotSubClass() throws IOException
+	{
+		File testEquivDir = new File(System.getProperty("user.dir") + "/build/test/testData/notSubClass");
+		Vocabulary vocab = new Vocabulary();
+		vocab.load(testEquivDir.getCanonicalPath(), null);
+
+		String[] allURIs = vocab.getAllURIs();
+		assertTrue("In total, there should only be 2 toplevel, unrelated classes.", allURIs.length == 2);
+		for (String uri : allURIs)
+			assertTrue("Unrecognized uri \"" + uri + "\"!", StringUtils.endsWith(uri, "A1") || StringUtils.endsWith(uri, "B1"));
+
+		Vocabulary.Hierarchy hier = vocab.getValueHierarchy();
+		assertTrue("Found hierarchy!", hier.rootBranches.size() == 0);
+	}
+
+	@Test
+	public void testFinalLabel() throws IOException
+	{
+		File testEquivDir = new File(System.getProperty("user.dir") + "/build/test/testData/finalLabel");
+		Vocabulary vocab = new Vocabulary();
+		vocab.load(testEquivDir.getCanonicalPath(), null);
+
+		String[] allURIs = vocab.getAllURIs();
+		assertTrue("In total, there should only be one toplevel class.", allURIs.length == 1);
+
+		String label = vocab.getLabel(allURIs[0]);
+		assertTrue("Expected final label to be \"Apple Bentley Charlie\"", label.equals("Apple Bentley Charlie"));
 	}
 }
