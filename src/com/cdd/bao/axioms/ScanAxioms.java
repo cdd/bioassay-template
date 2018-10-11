ee74a89777816531177d45c1bdbe88874dfeaf0a
Alex Clark <aclark.xyz@gmail.com>
2018-10-11 14:44:36 -0400

    Tidyup

--- src/com/cdd/bao/axioms/ScanAxioms.java
+++ src/com/cdd/bao/axioms/ScanAxioms.java
@@ -443,10 +443,10 @@ public class ScanAxioms
 						rule.impact = new Term[uriArray.length];
 						for (int n = 0; n < uriArray.length; n++) 
 						{
-							if (!(redundantURISet.contains("" + uriArray[n]))) 
+							if (!redundantURISet.contains("" + uriArray[n]))
 							{
-									rule.impact[n] = new Term(uriArray[n], true);
-									axvoc.addRule(rule);
+								rule.impact[n] = new Term(uriArray[n], true);
+								axvoc.addRule(rule);
 							}
 						}
 					}
@@ -492,10 +492,10 @@ public class ScanAxioms
 						rule.impact = new Term[uriArray.length];
 						for (int n = 0; n < uriArray.length; n++) 
 						{
-							if (!(redundantURISet.contains("" + uriArray[n]))) 
+							if (!redundantURISet.contains("" + uriArray[n]))
 							{
-									rule.impact[n] = new Term(uriArray[n], true);
-									axvoc.addRule(rule);
+								rule.impact[n] = new Term(uriArray[n], true);
+								axvoc.addRule(rule);
 							}
 						}
 					}
